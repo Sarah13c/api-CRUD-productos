19865255aa6e8bb288d62ad2c69e42e914931b4a
Sarah13c <sarahcb13@hotmail.com>
2024-03-06 13:19:48 -0500

    Publish

---
+++ PricatAPI/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace PricatAPI.Models
+{
+    public class Product
+    {
+        [Key]
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+        [Display(Name = "Imagen")]
+        public string ImageName { get; set; } = null!;
+
+        [Required]
+        [Display(Name = "Nombre")]
+        public string Name { get; set; } = null!;
+
+        [Display(Name = "Descripcion")]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        [Display(Name = "Precio")]
+        public decimal Price { get; set; }
+    }
+}
